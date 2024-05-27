199eeae8b61e292f40d2727ade9728a124e6ada7
Carlos Blanco <carlojbm04@gmail.com>
2024-05-27 20:28:36 +0200

    .

---
+++ CreateProject/Class/Program.cs
@@ -0,0 +1,19 @@
+﻿Sale sale = new Sale(15);
+var message = sale.getInfo();
+
+Console.WriteLine(message);
+
+class Sale
+{
+    public decimal Total {get; set;}
+
+    public Sale(decimal total)
+    {
+        this.Total = total;
+    }
+
+    public String getInfo()
+    {
+        return "El total es " + Total;
+    }
+}
