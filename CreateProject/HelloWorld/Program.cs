6574fdc7083a151c134ff9a5f4d4b8173a10e716
Carlos Blanco <carlojbm04@gmail.com>
2024-05-27 20:21:47 +0200

    Class and Objects

--- CreateProject/HelloWorld/Program.cs
+++ CreateProject/HelloWorld/Program.cs
@@ -1,2 +1,19 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Sale sale = new Sale(15);
+var message = sale.getInfo();
+
+Console.WriteLine(message);
+
+class Sale
+{
+    public decimal Total {get; set;}
+
+    public Sale(decimal total)
+    {
+        this.Total = total;
+    }
+
+    public String getInfo()
+    {
+        return "El total es " + Total;
+    }
+}
--- CreateProject/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ CreateProject/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+323d304674969f9f1e3ed2b81c9d3a303b40ec44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+303c67ef5b317ff225fd2e7edbdc8d5e949bca8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
