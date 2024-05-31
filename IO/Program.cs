5f97ff2b99b0f74bb237c442927c3d21bdeeb9c8
Carlos Blanco <carlojbm04@gmail.com>
2024-05-31 14:35:00 +0200

    Add bibliography

---
+++ IO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace myNameSpace
+{
+    public class IO
+    {
+        public void greet()
+        {
+            Console.WriteLine("What's your name");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+        }
+
+        public void takeATime()
+        {
+            Stopwatch crono = new Stopwatch();
+            crono.Start();
+            String message = Console.ReadLine();
+            crono.Stop();
+            Console.WriteLine(message.Length * 5 + "");
+        }
+        
+        private void Main(String[] args
+        {
+            greet();
+        }
+    }
+}
