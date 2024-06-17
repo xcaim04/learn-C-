63ce2f1e62ee1637ae48ab4e0693602e60946a46
Carlos Blanco <carlojbm04@gmail.com>
2024-06-17 15:54:46 +0200

    Learn OOP in c# and create a first class

---
+++ classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP
+{
+    class Car
+    {
+        string color = "red";
+
+        static void Main(string[] args)
+        {
+            Car volvo = new Car();
+            Console.WriteLine(volvo.color);
+        }
+
+    }
+}
